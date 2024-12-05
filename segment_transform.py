d10e7c75754ad8a1f36b91dfa33a9eb32608215f
YEzzo03 <youssef.asser@gmail.com>
2024-12-04 19:10:57 -0500

    color segmentation

--- segment_transform.py
+++ segment_transform.py
@@ -66,8 +66,8 @@ class CourtTransform:
         # print(bnd_box)
         xmin, ymin, xmax, ymax = map(int, bnd_box)
         roi = og_image[ymin:ymax, xmin:xmax]
-        print(og_image.shape)
-        print(roi)
+        print(og_image)
+        # print(roi)
         hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
         height, width, _ = roi.shape
 
